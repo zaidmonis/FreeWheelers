0a0c8e1f37ac1fc43b423fcccd4222c16a82dc45
Zaid <mzaid@onecallcm.com>
2022-02-04 16:19:36 +0530

    Add sample value endpoint

---
+++ FreeWheelers/Controllers/ValueController.cs
@@ -0,0 +1,16 @@
+namespace FreeWheelers.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetValues()
+        {
+            return new[] { "a", "b" };
+        }
+    }
+}
--- FreeWheelers/Startup.cs
+++ FreeWheelers/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,22 +17,14 @@ namespace FreeWheelers
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.AddControllers(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-            });
+            app.UseMvc();
         }
     }
 }
